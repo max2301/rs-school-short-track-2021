10f5666a7e1450d2ead2ad991607925b6d773b1a
max2301 <gitmax2301@gmail.com>
2021-04-09 19:53:01 +0300

    feat: add function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let k = 1;
+  const res = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      k++;
+    } else if (k !== 1) {
+      res.push(`${k}${str[i]}`);
+      k = 1;
+    } else {
+      res.push(str[i]);
+    }
+  }
+  return res.join('');
 }
 
 module.exports = encodeLine;
